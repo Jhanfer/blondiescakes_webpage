a78d37e7072468e0b94c113f5b4d3945bc0abe36
Jhanfer <jhafer73@gmail.com>
2024-06-20 17:15:33 +0200

    Backend test

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="blondiescakes_webpage",
-    api_url="https://blondiescakes-webpage-ln1r0hecf-jhanfers-projects.vercel.app"
+    app_name="blondiescakes_webpage"
 )
